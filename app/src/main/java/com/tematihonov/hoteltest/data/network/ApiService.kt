e6fab05419222dd958e7e9c6924edc0bfab5e925
Artem Tikhonov <temaTihonov@yandex.ru>
2023-09-01 23:34:16 +0300

    Finish hotel screen w/o 1 moment

--- app/src/main/java/com/tematihonov/hoteltest/data/network/ApiService.kt
+++ app/src/main/java/com/tematihonov/hoteltest/data/network/ApiService.kt
@@ -4,12 +4,13 @@ import com.tematihonov.hoteltest.data.models.responceBooking.Booking
 import com.tematihonov.hoteltest.data.models.responceRooms.Room
 import com.tematihonov.hoteltest.data.models.responseHotel.Hotel
 import com.tematihonov.hoteltest.utils.RetrofitConstants
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET(RetrofitConstants.HOTEL)
-    suspend fun getHotel(): Hotel
+    suspend fun getHotel(): Response<Hotel>
 
     @GET(RetrofitConstants.ROOMS)
     suspend fun getRooms(): List<Room>
--- app/src/main/java/com/tematihonov/hoteltest/presentation/viewmodel/HotelViewModel.kt
+++ app/src/main/java/com/tematihonov/hoteltest/presentation/viewmodel/HotelViewModel.kt
@@ -1,8 +1,9 @@
 package com.tematihonov.hoteltest.presentation.viewmodel
 
-import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tematihonov.hoteltest.data.models.responseHotel.Hotel
 import com.tematihonov.hoteltest.domain.usecase.NetworkUseCases
 import kotlinx.coroutines.launch
 
@@ -10,14 +11,42 @@ class HotelViewModel(
     private val networkUseCases: NetworkUseCases
 ): ViewModel() {
 
+    val hotelModel = MutableLiveData<Hotel>()
+    val isLoading = MutableLiveData<Boolean>()
+
+//    private val _hotel = MutableLiveData<Resource<Hotel>>()
+//    val hotel: LiveData<Resource<Hotel>> = _hotel
+
+
     var arb = mutableListOf<Int>(1,2,3)
 
-    fun initHotel() {
-        viewModelScope.launch {
-            var hotel = networkUseCases.getHotelUseCase.invoke()
-            Log.d("GGG", "${hotel.adress}")
-        }
-        Log.d("GGG", "123")
+    init {
+        loadHotel()
     }
+
+    fun loadHotel() {
+        viewModelScope.launch {
+//            _hotel.postValue(Resource.Loading())
+//            _hotel.postValue(networkUseCases.getHotelUseCase.invoke())
+
+            isLoading.postValue(true)
+            val result = networkUseCases.getHotelUseCase.invoke()
+            if (result.isSuccessful) {
+                hotelModel.postValue(result.body())
+                isLoading.postValue(false)
+            }
+        }
+    }
+
+
+//    data class UiState(
+//        val isLoading: Boolean = false,
+//        val error: Error? = null,
+//        val item: Hotel? = null
+//    ) {
+//        sealed class Error {
+//            object NetworkError : Error()
+//        }
+//    }
 
 }
