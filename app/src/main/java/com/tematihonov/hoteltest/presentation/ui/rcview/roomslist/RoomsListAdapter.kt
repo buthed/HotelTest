056b6b79ab5b01ecbfd6e7aea2e1da83f246729c
Artem Tikhonov <temaTihonov@yandex.ru>
2023-09-02 16:15:53 +0300

    Carousel with coil for Room screen

--- app/src/main/java/com/tematihonov/hoteltest/presentation/ui/rcview/roomslist/RoomsListAdapter.kt
+++ app/src/main/java/com/tematihonov/hoteltest/presentation/ui/rcview/roomslist/RoomsListAdapter.kt
@@ -1,15 +1,22 @@
 package com.tematihonov.hoteltest.presentation.ui.rcview.roomslist
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.tematihonov.hoteltest.R
 import com.tematihonov.hoteltest.data.models.responceRooms.Room
 import com.tematihonov.hoteltest.databinding.ItemRoomBinding
+import com.tematihonov.hoteltest.presentation.ui.rcview.hotelimageslist.HotelImagesAdapter
 
 class RoomsListAdapter() : RecyclerView.Adapter<RoomsListAdapter.RoomsListViewHolder>() {
 
     private lateinit var rListener: onItemClickListener
+    private lateinit var context: Context
 
     interface onItemClickListener {
 
@@ -33,6 +40,7 @@ class RoomsListAdapter() : RecyclerView.Adapter<RoomsListAdapter.RoomsListViewHo
         parent: ViewGroup,
         viewType: Int,
     ): RoomsListAdapter.RoomsListViewHolder {
+        context = parent.context
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemRoomBinding.inflate(inflater, parent, false)
         return RoomsListViewHolder(binding)
@@ -47,8 +55,63 @@ class RoomsListAdapter() : RecyclerView.Adapter<RoomsListAdapter.RoomsListViewHo
             buttonChooseRoom.setOnClickListener {
                 rListener.onClick(position)
             }
-            //TODO add carousel
+            roomCarousel(room.image_urls, holder.binding)
             //TODO add peculiarities
+        }
+    }
+
+    fun roomCarousel(imageUrls: List<String>, binding: ItemRoomBinding) {
+        var viewPager2: ViewPager2 = binding.carousel
+        val images = listOf(imageUrls[0], imageUrls[1], imageUrls[2])
+        val adapter = HotelImagesAdapter(images)
+        viewPager2.adapter = adapter
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int,
+            ) {
+                changeColor(binding, viewPager2)
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+                changeColor(binding, viewPager2)
+            }
+        })
+    }
+
+    fun changeColor(binding: ItemRoomBinding, viewPager2: ViewPager2) {
+        when (viewPager2.currentItem) {
+            0 -> {
+                binding.hotelCarouselCv1.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.black))
+                binding.hotelCarouselCv2.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                binding.hotelCarouselCv3.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                Log.d("GGG", "Carousel 0")
+            }
+            1 -> {
+                binding.hotelCarouselCv1.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                binding.hotelCarouselCv2.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.black))
+                binding.hotelCarouselCv3.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                Log.d("GGG", "Carousel 1")
+            }
+            2 -> {
+                binding.hotelCarouselCv1.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                binding.hotelCarouselCv2.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.carousel_grey))
+                binding.hotelCarouselCv3.backgroundTintList = ColorStateList.valueOf(context.resources.getColor(
+                    R.color.black))
+                Log.d("GGG", "Carousel 2")
+            } //TODO del deprecaited
         }
     }
 
